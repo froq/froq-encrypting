1d67d430183a2ae9321998ca202f36fbaa3ced75
Kerem Güneş <k-gun@mail.com>
2019-01-29 22:27:37 +0300

    Add Twoway\OpenSsl object.

---
+++ src/Twoway/OpenSsl.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * MIT License <https://opensource.org/licenses/mit>
+ *
+ * Copyright (c) 2015 Kerem Güneş
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+declare(strict_types=1);
+
+namespace Froq\Encryption\Twoway;
+
+use Froq\Encryption\EncryptionException;
+
+/**
+ * @package    Froq
+ * @subpackage Froq\Encryption
+ * @object     Froq\Encryption\Twoway\OpenSsl
+ * @author     Kerem Güneş <k-gun@mail.com>
+ * @since      3.0
+ */
+final class OpenSsl extends Twoway
+{
+    /*** Original source: https://stackoverflow.com/a/30189841/362780 ***/
+
+    /**
+     * Method.
+     * @const string
+     */
+    public const METHOD = 'aes-256-ctr';
+
+    /**
+     * Method.
+     * @var string
+     */
+    private $method;
+
+    /**
+     * Constructor.
+     * @param  string      $key
+     * @param  string|null $method
+     * @throws Froq\Encryption\EncryptionException
+     */
+    public function __construct(string $key, string $method = null)
+    {
+        if (!extension_loaded('openssl')) {
+            throw new EncryptionException('OpenSSL extension not found');
+        }
+
+        $this->key = $key;
+        $this->method = $method ?? self::METHOD;
+
+        // check key length
+        if (strlen($this->key) < 16) {
+            throw new EncryptionException("Invalid key given, minimum key length is 16 (tip: use ".
+                "OpenSSL::generateKey() method to get a strong key)");
+        }
+        // check method validity
+        if (!in_array($this->method, openssl_get_cipher_methods())) {
+            throw new EncryptionException("Invalid method '{$this->method}' given");
+        }
+    }
+
+    /**
+     * Get method.
+     * @return string
+     */
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    /**
+     * @inheritDoc Froq\Encryption\Twoway\Twoway
+     */
+    public function encode(string $data): ?string
+    {
+        [$eKey, $aKey] = $this->keys();
+
+        $out = openssl_encrypt($data, $this->method, $eKey, OPENSSL_RAW_DATA,
+            $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($this->method)));
+        $out = $iv . $out;
+
+        $mac = hash_hmac('sha256', $out, $aKey, true);
+        $out = $mac . $out;
+
+        return (string) base64_encode($out);
+    }
+
+    /**
+     * @inheritDoc Froq\Encryption\Twoway\Twoway
+     */
+    public function decode(string $data): ?string
+    {
+        [$eKey, $aKey] = $this->keys();
+
+        $data = base64_decode($data, true);
+
+        $macLen = mb_strlen(hash('sha256', '', true), '8bit');
+        $mac = mb_substr($data, 0, $macLen, '8bit');
+        $data = mb_substr($data, $macLen, null, '8bit');
+
+        // validate hashes
+        if (!hash_equals($mac, hash_hmac('sha256', $data, $aKey, true))) {
+            return null;
+        }
+
+        $ivLen = openssl_cipher_iv_length($this->method);
+        $iv = mb_substr($data, 0, $ivLen, '8bit');
+        $data = mb_substr($data, $ivLen, null, '8bit');
+
+        return (string) openssl_decrypt($data, $this->method, $eKey, OPENSSL_RAW_DATA, $iv);
+    }
+
+    /**
+     * Keys.
+     * @return array
+     */
+    private function keys(): array
+    {
+        return [
+            hash_hmac('sha256', 'ENCRYPTION', $this->key, true),
+            hash_hmac('sha256', 'AUTHENTICATION', $this->key, true)
+        ];
+    }
+}
