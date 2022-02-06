6932ac36d8bdb17cf172c88bfb91469e3dde95b0
Kerem Güneş <qeremy@gmail.com>
2022-02-06 15:54:52 +0300

    * Hash: make-ups.

--- src/Hash.php
+++ src/Hash.php
@@ -7,12 +7,10 @@ declare(strict_types=1);
 
 namespace froq\encrypting;
 
-use froq\encrypting\EncryptingException;
-
 /**
  * Hash.
  *
- * Represents a static class which is able to generate hashes by given lengths.
+ * A static class that generates hashes by given lengths.
  *
  * @package froq\encrypting
  * @object  froq\encrypting\Hash
@@ -22,33 +20,34 @@ use froq\encrypting\EncryptingException;
  */
 final class Hash
 {
-    /**
-     * Algos.
-     * @const array<int, string>
-     */
-    public const ALGOS = [40 => 'sha1', 8 => 'fnv1a32', 16 => 'fnv1a64', 32 => 'md5',
-        64 => 'sha256', 128 => 'sha512'];
+    /** @const array<int, string> */
+    public const ALGOS = [
+        40 => 'sha1', 8  => 'fnv1a32', 16  => 'fnv1a64',
+        32 => 'md5',  64 => 'sha256',  128 => 'sha512'
+    ];
 
     /**
-     * Make an hash by given length.
+     * Make a hash by given length.
      *
-     * @param  string     $in
+     * @param  string     $input
      * @param  int        $length
      * @param  array|null $lengths @internal
      * @return string
      * @throws froq\encrypting\EncryptingException
      */
-    public static function make(string $in, int $length, array $lengths = null): string
+    public static function make(string $input, int $length, array $lengths = null): string
     {
-        $lengths = $lengths ?? array_keys(self::ALGOS);
+        $lengths ??= array_keys(self::ALGOS);
 
         if (!in_array($length, $lengths, true)) {
-            throw new EncryptingException('Invalid length value `%s`, valids are: %s',
-                [$length, join(', ', $lengths)]);
+            throw new EncryptingException(
+                'Invalid length value `%s` [valids: %s]',
+                [$length, join(', ', $lengths)
+            ]);
         }
 
         $algo = self::ALGOS[$length];
 
-        return hash($algo, $in);
+        return hash($algo, $input);
     }
 }
