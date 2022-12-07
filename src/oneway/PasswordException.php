24afe4c83e730afdc4ac532fb9e5d501bd4736dc
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2022-12-07 22:46:48 +0300

    Add oneway/twoway exceptions.

---
+++ src/oneway/PasswordException.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-encrypting
+ */
+namespace froq\encrypting\oneway;
+
+/**
+ * @package froq\encrypting\oneway
+ * @class   froq\encrypting\oneway\PasswordException
+ * @author  Kerem Güneş
+ * @since   7.0
+ */
+class PasswordException extends OnewayException
+{}
---
+++ src/twoway/CrypteeException.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-encrypting
+ */
+namespace froq\encrypting\twoway;
+
+/**
+ * @package froq\encrypting\twoway
+ * @class   froq\encrypting\twoway\CrypteeException
+ * @author  Kerem Güneş
+ * @since   7.0
+ */
+class CrypteeException extends TwowayException
+{}
